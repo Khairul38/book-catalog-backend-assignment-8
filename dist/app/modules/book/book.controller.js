80a8d2dea24dbbd740c0d60b20b66e0ff2561a6d
KHAIRUL ISLAM <khairulislam38@gmail.com>
2023-09-03 22:47:26 +0600

    fix some bug and update readme

--- dist/app/modules/book/book.controller.js
+++ dist/app/modules/book/book.controller.js
@@ -1,29 +1,67 @@
 "use strict";
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+var __awaiter =
+  (this && this.__awaiter) ||
+  function (thisArg, _arguments, P, generator) {
+    function adopt(value) {
+      return value instanceof P
+        ? value
+        : new P(function (resolve) {
+            resolve(value);
+          });
+    }
     return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
+      function fulfilled(value) {
+        try {
+          step(generator.next(value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function rejected(value) {
+        try {
+          step(generator["throw"](value));
+        } catch (e) {
+          reject(e);
+        }
+      }
+      function step(result) {
+        result.done
+          ? resolve(result.value)
+          : adopt(result.value).then(fulfilled, rejected);
+      }
+      step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
-};
-var __rest = (this && this.__rest) || function (s, e) {
+  };
+var __rest =
+  (this && this.__rest) ||
+  function (s, e) {
     var t = {};
-    for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
+    for (var p in s)
+      if (Object.prototype.hasOwnProperty.call(s, p) && e.indexOf(p) < 0)
         t[p] = s[p];
     if (s != null && typeof Object.getOwnPropertySymbols === "function")
-        for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
-            if (e.indexOf(p[i]) < 0 && Object.prototype.propertyIsEnumerable.call(s, p[i]))
-                t[p[i]] = s[p[i]];
-        }
+      for (var i = 0, p = Object.getOwnPropertySymbols(s); i < p.length; i++) {
+        if (
+          e.indexOf(p[i]) < 0 &&
+          Object.prototype.propertyIsEnumerable.call(s, p[i])
+        )
+          t[p[i]] = s[p[i]];
+      }
     return t;
-};
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+  };
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.deleteSingleBook = exports.updateSingleBook = exports.getSingleBook = exports.getAllBookByCategory = exports.getAllBook = exports.createBook = void 0;
+exports.deleteSingleBook =
+  exports.updateSingleBook =
+  exports.getSingleBook =
+  exports.getAllBookByCategory =
+  exports.getAllBook =
+  exports.createBook =
+    void 0;
 const http_status_1 = __importDefault(require("http-status"));
 const pagination_1 = require("../../../constants/pagination");
 const catchAsync_1 = __importDefault(require("../../../shared/catchAsync"));
@@ -31,67 +69,98 @@ const pick_1 = __importDefault(require("../../../shared/pick"));
 const sendResponse_1 = __importDefault(require("../../../shared/sendResponse"));
 const book_constant_1 = require("./book.constant");
 const book_service_1 = require("./book.service");
-exports.createBook = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+exports.createBook = (0, catchAsync_1.default)((req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     const book = __rest(req.body, []);
     const result = yield (0, book_service_1.createBookToDB)(book);
     (0, sendResponse_1.default)(res, {
-        success: true,
-        statusCode: http_status_1.default.OK,
-        message: "Book created successfully",
-        data: result,
+      success: true,
+      statusCode: http_status_1.default.OK,
+      message: "Book created successfully",
+      data: result,
     });
-}));
-exports.getAllBook = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const filters = (0, pick_1.default)(req.query, book_constant_1.bookFilterableFields);
-    const pagination = (0, pick_1.default)(req.query, pagination_1.paginationFields);
-    const result = yield (0, book_service_1.getAllBookFromDB)(filters, pagination);
+  })
+);
+exports.getAllBook = (0, catchAsync_1.default)((req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
+    const filters = (0, pick_1.default)(
+      req.query,
+      book_constant_1.bookFilterableFields
+    );
+    const pagination = (0, pick_1.default)(
+      req.query,
+      pagination_1.paginationFields
+    );
+    const result = yield (0, book_service_1.getAllBookFromDB)(
+      filters,
+      pagination
+    );
     (0, sendResponse_1.default)(res, {
-        statusCode: http_status_1.default.OK,
-        success: true,
-        message: "Books fetched successfully",
-        meta: result.meta,
-        data: result.data,
+      statusCode: http_status_1.default.OK,
+      success: true,
+      message: "Books fetched successfully",
+      meta: result.meta,
+      data: result.data,
     });
-}));
-exports.getAllBookByCategory = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    const filters = (0, pick_1.default)(req.query, book_constant_1.bookFilterableFields);
-    const pagination = (0, pick_1.default)(req.query, pagination_1.paginationFields);
-    const result = yield (0, book_service_1.getAllBookByCategoryFromDB)(filters, pagination, req.params.categoryId);
+  })
+);
+exports.getAllBookByCategory = (0, catchAsync_1.default)((req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
+    const filters = (0, pick_1.default)(
+      req.query,
+      book_constant_1.bookFilterableFields
+    );
+    const pagination = (0, pick_1.default)(
+      req.query,
+      pagination_1.paginationFields
+    );
+    const result = yield (0, book_service_1.getAllBookByCategoryFromDB)(
+      filters,
+      pagination,
+      req.params.categoryId
+    );
     (0, sendResponse_1.default)(res, {
-        statusCode: http_status_1.default.OK,
-        success: true,
-        message: "Books with associated category data fetched successfully",
-        meta: result.meta,
-        data: result.data,
+      statusCode: http_status_1.default.OK,
+      success: true,
+      message: "Books with associated category data fetched successfully",
+      meta: result.meta,
+      data: result.data,
     });
-}));
-exports.getSingleBook = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+  })
+);
+exports.getSingleBook = (0, catchAsync_1.default)((req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     const result = yield (0, book_service_1.getSingleBookFromDB)(req.params.id);
     (0, sendResponse_1.default)(res, {
-        statusCode: http_status_1.default.OK,
-        success: true,
-        message: "Single book fetched successfully",
-        data: result,
+      statusCode: http_status_1.default.OK,
+      success: true,
+      message: "Single book fetched successfully",
+      data: result,
     });
-}));
-exports.updateSingleBook = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+  })
+);
+exports.updateSingleBook = (0, catchAsync_1.default)((req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
     const payload = req.body;
     const result = yield (0, book_service_1.updateSingleBookToDB)(id, payload);
     (0, sendResponse_1.default)(res, {
-        statusCode: http_status_1.default.OK,
-        success: true,
-        message: "Book updated successfully",
-        data: result,
+      statusCode: http_status_1.default.OK,
+      success: true,
+      message: "Book updated successfully",
+      data: result,
     });
-}));
-exports.deleteSingleBook = (0, catchAsync_1.default)((req, res) => __awaiter(void 0, void 0, void 0, function* () {
+  })
+);
+exports.deleteSingleBook = (0, catchAsync_1.default)((req, res) =>
+  __awaiter(void 0, void 0, void 0, function* () {
     const { id } = req.params;
     const result = yield (0, book_service_1.deleteSingleBookFromDB)(id);
     (0, sendResponse_1.default)(res, {
-        statusCode: http_status_1.default.OK,
-        success: true,
-        message: "Book deleted successfully",
-        data: result,
+      statusCode: http_status_1.default.OK,
+      success: true,
+      message: "Book deleted successfully",
+      data: result,
     });
-}));
+  })
+);
