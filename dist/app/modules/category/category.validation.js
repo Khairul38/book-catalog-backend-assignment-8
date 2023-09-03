f6d0211d7f007e664b5f1f3a69933e15d95c2090
KHAIRUL ISLAM <khairulislam38@gmail.com>
2023-09-03 22:48:56 +0600

    Done build

--- dist/app/modules/category/category.validation.js
+++ dist/app/modules/category/category.validation.js
@@ -3,14 +3,14 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.updateCategoryZodSchema = exports.createCategoryZodSchema = void 0;
 const zod_1 = require("zod");
 exports.createCategoryZodSchema = zod_1.z.object({
-  body: zod_1.z.object({
-    title: zod_1.z.string({
-      required_error: "Title is required",
+    body: zod_1.z.object({
+        title: zod_1.z.string({
+            required_error: "Title is required",
+        }),
     }),
-  }),
 });
 exports.updateCategoryZodSchema = zod_1.z.object({
-  body: zod_1.z.object({
-    title: zod_1.z.string().optional(),
-  }),
+    body: zod_1.z.object({
+        title: zod_1.z.string().optional(),
+    }),
 });
